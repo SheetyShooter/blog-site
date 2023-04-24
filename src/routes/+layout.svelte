31ed67e3ca95bcd350ad1f2f57d2e06a4f02762a
BuildTools <unconfigured@null.spigotmc.org>
2023-04-24 17:22:48 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<nav>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+</nav>
+
+<slot />
+
+<style>
+    nav {
+        background: rgb(59, 255, 255);
+        height: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 40px;
+    }
+
+    a {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 1.2em;
+        font-weight: 200;
+        color: #fff;
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+    let blogs = [
+        {
+            id: 1,
+            title: 'my first blog',
+            blogBody: 'This is my first blog.',
+            datePosted: '4/24/2023'
+        },
+        {
+            id: 2,
+            title: 'my second blog',
+            blogBody: 'This is my second blog.  I will keep writing things to fill up space.',
+            datePosted: '4/24/2023'
+        }
+    ];
+</script>
+
+<div class="main-wrapper">
+    <h1 class="main-header">Blog Posts</h1>
+    <div class="blog-wrapper">
+        {#each blogs as post (post.id)}
+          <div class="blog-post-card">
+            <h3>{post.title}</h3>
+            <p>{post.datePosted}</p>
+            <p>{post.blogBody}</p>
+          </div>
+        {/each}
+    </div>
+</div>
+
+
+<style>
+    .main-wrapper {
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    .main-header {
+        text-align: center;
+    }
+
+    .blog-wrapper {
+      width: 60vw;
+      margin: 100px auto 0;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .blog-post-card {
+        background: rgb(207, 207, 207);
+        margin: 10px;
+        padding: 5px;
+        border-radius: 10px;
+    }
+</style>
